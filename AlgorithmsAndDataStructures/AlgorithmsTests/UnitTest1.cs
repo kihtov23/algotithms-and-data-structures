0de23cf3e60aca85276dfad1c0a694aa6d38cb49
Oleh Kikhtov <kihtov23@gmail.com>
2022-08-12 22:11:34 +0300

    add course again

---
+++ AlgorithmsAndDataStructures/AlgorithmsTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace AlgorithmsTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ AlgorithmsAndDataStructures/DataStructuresTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace DataStructuresTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
