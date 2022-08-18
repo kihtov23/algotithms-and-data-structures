f9d0c9a3164f7159f0448bc7ab446df1aef84728
Oleh Kikhtov <kihtov23@gmail.com>
2022-08-18 17:11:06 +0300

    Insertion sort

--- AlgorithmsAndDataStructures/Algorithms/Sorting.cs
+++ AlgorithmsAndDataStructures/Algorithms/Sorting.cs
@@ -7,29 +7,27 @@
         // 4 [6]<>[7] 2 ->
         // 4 6 [7]<>[2] ->
         // 4 6 2 | 7
-        public static int[] BubbleSort(int[] arr)
+        public static void BubbleSort(int[] arr)
         {
-            for (int partIndex = arr.Length-1; partIndex > 0; partIndex--)
+            for (int partIndex = arr.Length - 1; partIndex > 0; partIndex--)
             {
                 for (int j = 0; j < partIndex; j++)
                 {
-                    if (arr[j] > arr[j+1])
+                    if (arr[j] > arr[j + 1])
                     {
                         Swap(arr, j, j + 1);
                     }
                 }
             }
-            return arr;
         }
 
         //  arr = 6 4 7 2
-        // store index if max [6]<>4 7 2 => 2 4 7 | 6
-        public static int[] SelectionSort(int[] arr)
+        // loop and store index of max element [6]<>4 7 2 => 2 4 7 | 6
+        public static void SelectionSort(int[] arr)
         {
-            int maxIndex = 0; 
-            for (int partIndex = arr.Length-1; partIndex > 0; partIndex--)
+            for (int partIndex = arr.Length - 1; partIndex > 0; partIndex--)
             {
-                maxIndex = 0;
+                int maxIndex = 0;
                 for (int i = 0; i <= partIndex; i++)
                 {
                     if (arr[i] > arr[maxIndex])
@@ -39,7 +37,27 @@
                 }
                 Swap(arr, maxIndex, partIndex);
             }
-            return arr;
+        }
+
+        // arr = 6 4 7 2
+        // 6 | 4 7 2, temp = 4, shift left array until temp < el
+        // 4 6 | 7 2, if 7 > 6, shift partition
+        // 4 6 7 | 2, temp = 2, shift 7, shift, 6, shift 4, -> 2 4 6 7
+        public static void InsertionSort(int[] arr)
+        {
+            //min edge case: partIndex=1, arr[partIndex]=4, i->0..0 arr[i] 6
+            //middle         partIndex=2, arr[partIndex]=7, i->1..0 arr[i] 6, 4
+            //max edge case: partIndex=3, arr[partIndex]=2, i->2..0 arr[i] 7, 6, 4
+            for (int partIndex = 1; partIndex < arr.Length; partIndex++)
+            {
+                int currUnsorted = arr[partIndex];
+                int i = 0;
+                for (i = partIndex; i > 0 && arr[i - 1] > currUnsorted; i--)
+                {
+                    arr[i] = arr[i - 1];
+                }
+                arr[i] = currUnsorted;
+            }
         }
 
         private static void Swap(int[] arr, int a, int b)
--- AlgorithmsAndDataStructures/AlgorithmsTests/SortingTests.cs
+++ AlgorithmsAndDataStructures/AlgorithmsTests/SortingTests.cs
@@ -9,24 +9,68 @@ namespace AlgorithmsTests
         {
         }
 
+        private int[][] Samples()
+        {
+            int[][] samples = new int[9][];
+            samples[0] = new[] { 1 };
+            samples[1] = new[] { 2, 1 };
+            samples[2] = new[] { 2, 1, 3 };
+            samples[3] = new[] { 1, 1, 1 };
+            samples[4] = new[] { 2, -1, 3, 3 };
+            samples[5] = new[] { 4, -5, 3, 3 };
+            samples[6] = new[] { 0, -5, 3, 3 };
+            samples[7] = new[] { 0, -5, 3, 0 };
+            samples[8] = new[] { 3, 2, 5, 5, 1, 0, 7, 8 };
+
+            return samples;
+        }
+
+        private void RunTestsForSortingAlgorithms(Action<int[]> sortingFunction)
+        {
+            foreach(var sample in Samples())
+            {
+                PrintBefore(sample);
+                sortingFunction(sample);
+                CollectionAssert.IsOrdered(sample);
+                PrintAfter(sample);
+            }
+        }
+
+        private void PrintBefore(int[] arr)
+        {
+            TestContext.Write("Array before sorting: ");
+            foreach (var item in arr)
+            {
+                TestContext.Write($" {item} ");
+            }
+        }
+
+        private void PrintAfter(int[] arr)
+        {
+            TestContext.Write("Array after sorting: ");
+            foreach (var item in arr)
+            {
+                TestContext.Write($" {item} ");
+            }
+            TestContext.Write($"\n");
+        }
+
         [Test]
         public void BubbleSort_Returns_SortedArray()
         {
-            //Act
-            var sortedArray = Sorting.BubbleSort(new int[] { 6, 4, 7, 2 });
-
-            //Assert
-            CollectionAssert.IsOrdered(sortedArray);
+            RunTestsForSortingAlgorithms(Sorting.BubbleSort);
         }
 
         [Test]
         public void SelectionSort_Returns_SortedArray()
         {
-            //Act 
-            var sortedArray = Sorting.SelectionSort(new int[] { 6, 4, 7, 2 });
+            RunTestsForSortingAlgorithms(Sorting.SelectionSort);
+        }
 
-            //Assert
-            CollectionAssert.IsOrdered(sortedArray);
+        [Test]
+        public void InsertionSort_Returns_SortedArray()
+        {
+            RunTestsForSortingAlgorithms(Sorting.InsertionSort);
         }
     }
 }
